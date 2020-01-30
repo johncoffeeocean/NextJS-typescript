c245d17e2652fa6bcdd6df86e2f684d8d8d709c5
johncoffeeocean <coffeeocean918@outlook.com>
2020-01-30 21:27:40 +0800

    _app.js to _app.tsx

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
 const dotEnvResult = require("dotenv").config();
 
-// const prod = process.env.NODE_ENV === 'production'
-
 if (dotEnvResult.error) {
   throw dotEnvResult.error;
 }
-
-// module.exports = {
-//   env: {
-//     TEST: process.env.TEST,
-//     BACKEND_URL: prod ? 'https://api.example.com' : 'https://localhost:8080',
-//   },
-// }
-
-/*
-  If you want to include every variable in .env automatically,
-  replace the above module.exports with the code below.
-  Warning: The below technique can be dangerous since it exposes every
-  variable in .env to the client. Even if you do not currently have
-  sensitive information there, it can be easy to forget about this when
-  you add variables in the future.
-  If you have many variables and want a safer way to conveniently expose
-  them, see the example "with-dotenv".
-*/
 
 const parsedVariables = dotEnvResult.parsed || {};
 const dotEnvVariables = {};
