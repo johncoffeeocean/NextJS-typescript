7e9e04dde3240dcbb24298090563a12715a6605d
johncoffeeocean <coffeeocean918@outlook.com>
2020-02-11 12:12:21 +0800

    Moved to Next.js repo

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
   }
 };
